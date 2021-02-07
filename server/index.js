b297d6feeca7a5aa3010ac857a59b57573c33568
Elijah Strauss <emstraus@buffalo.edu>
2021-02-07 18:18:50 -0500

    View Counter attempt. Count is not defined error

--- server/index.js
+++ server/index.js
@@ -2,17 +2,34 @@ const express = require('express');
 const ejs = require ('ejs');
 const path = require ('path');
 
-const clientPath = path.join(__dirname,'../client')
-const staticPath = path.join(clientPath,'/static');
-const viewPath = path.join(clientPath,'/views')
+const clientPath = path.join(__dirname,'../client/')
+const staticPath = path.join(clientPath,'/static/');
+const viewPath = path.join(clientPath,'/views/')
+
 const app = express();
+
 app.set ('view engine','ejs');
 app.set ('views', viewPath);
+
+var x=0;
+
+const counter= function(req,res,next){
+    x++;
+    console.log(x);
+    next();
+}
+
+//app.use (counter);
+app.use (express.static(staticPath));
 
 app.get ('/', function (req, res){
     res.render('index');
 });
 
-app.use (express.static(staticPath));
+app.get ('/slime', counter, function (req, res){
+    res.render('slime');
+});
+
+
 
 app.listen(2000);
