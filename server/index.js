4d65e9ec9c820e07f62c2fd10b059948c947b314
Elijah Strauss <emstraus@buffalo.edu>
2021-02-07 17:40:03 -0500

    made node.js views engine

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const ejs = require ('ejs');
+const path = require ('path');
+
+const clientPath = path.join(__dirname,'../client')
+const staticPath = path.join(clientPath,'/static');
+const viewPath = path.join(clientPath,'/views')
+const app = express();
+app.set ('view engine','ejs');
+app.set ('views', viewPath);
+
+app.get ('/', function (req, res){
+    res.render('index');
+});
+
+app.use (express.static(staticPath));
+
+app.listen(2000);
