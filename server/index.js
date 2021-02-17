e932e5a5c78834e7531bb150d2c9b459b2fbdecb
Me0wsme0w <emstraus@buffalo.edu>
2021-02-16 21:37:12 -0500

    cookies

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const ejs = require ('ejs');
 const path = require ('path');
+const { response } = require('express');
+const bodyParser = require('body-parser');
+const session = require ('express-session')
 
 const clientPath = path.join(__dirname,'../client/')
 const staticPath = path.join(clientPath,'/static/');
@@ -21,15 +24,31 @@ const counter= function(req,res,next){
 
 //app.use (counter);
 app.use (express.static(staticPath));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    name: 'slimes',
+    secret: 'eachdogneeds15cats',
+    saveUninitialized: true,
+    resave: true,
+    cookie: {
+        maxAge: 1000*60*60*24*3,
+    }
+}));
+
+//routes
+
 
 app.get ('/', function (req, res){
-    res.render('index');
+    res.render('index', {nomen: req.session.username});
 });
 
-app.get ('/slime', counter, function (req, res){
-    res.render('slime',{count:x});
+app.get ('/slime', function (req, res){
+    res.render('slime', {nomen: req.session.username});
 });
 
-
+app.post ('/welcome' , (req, res)=>{
+req.session.username=req.body.visitorname;
+res.redirect('/');
+})
 
 app.listen(2000);
