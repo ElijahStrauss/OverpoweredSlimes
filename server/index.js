2e3303d2a9142e8476e88135576278502380739e
ElijahStrauss <emstraus@buffalo.edu>
2021-05-13 19:08:35 -0400

    tutorial 5 complete

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const ejs = require ('ejs');
 const path = require ('path');
 const { response } = require('express');
-const bodyParser = require('body-parser');
 const session = require ('express-session')
 const mongoose = require('mongoose');
-const {BlogPost} = require('./models.js')
+const {BlogPost, User} = require('./models.js')
+const bcrypt = require('bcrypt');
 
 const clientPath = path.join(__dirname,'../client/')
 const staticPath = path.join(clientPath,'/static/');
@@ -26,7 +26,7 @@ const counter= function(req,res,next){
 
 //app.use (counter);
 app.use (express.static(staticPath));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(session({
     name: 'slimes',
     secret: 'eachdogneeds15cats',
@@ -40,6 +40,15 @@ app.use(session({
 mongoose.connect('mongodb://localhost:27017/slime',{useNewUrlParser: true});
 
 //routes
+const authenticated = function(req, res, next) {
+    if(req.session.authenticated) next();
+    else res.redirect('/login');
+}
+
+const slimelover = function(req, res, next) {
+    if(req.session.isSlimeLover) next();
+    else res.send('BEGONE, YOU DEGENERATE.');
+}
 
 
 app.get ('/',  (req, res)=>{
@@ -49,43 +58,49 @@ app.get ('/',  (req, res)=>{
 app.get ('/slime', (req, res)=>{
     res.render('slime', {data: req.session});
 });
-app.get ('/blog/', async (req, res) =>{
-    var posts= BlogPost.find({}, (error, result)=>{
-        if(error){
+app.get('/blog/', async (req, res)=>{
+    var posts = await BlogPost.find({}, (error, result) => {
+        if(error) {
             console.log(error);
             res.sendStatus(500);
         }
         console.log(result);
         res.render('blog', {data: req.session, postset: result});
     });
+
 });
 
 app.get('/blog/write/', (req, res)=>{
     res.render('writing', {data: req.session, draft: {}});
 });
 
-app.get('/blog/:id', async (req,res) =>{
+app.get('/blog/:id/', (req,res) => {
     var searchID = req.params.id;
-    BlogPost.findById({_id: searchID}, (error, result)=>{
-        if(error){
-            res.redirect('/blog');
+    BlogPost.findById(searchID, (error, result)=>{
+        if(error) {
+            console.log(error);
+            res.redirect('/blog/');
         }
-        else if(!result) res.redirect('/blog/');
-        else{
-            res.render('entry', {data: req.session, entry: result});
-        }   
+        else if(!result) {
+            res.status(404);
+        }
+        else {
+            console.log(result)
+            let parsedText = result.body.replace(/\r\n|\r|\n/g,"<br />");
+            result.parsedText = parsedText;
+            res.render('entry',{data: req.session, entry: result});
+        }
     })
-})
-
-app.post('/blog/writepost', async (req, res)=>{
+});
+app.post('/blog/writepost', authenticated, slimelover, async (req, res)=>{
     console.log(req.body);
     try {
         let newPost = new BlogPost(req.body);
+        newPost.author = req.session.username;
         await newPost.save();
         res.redirect('/blog/');
     }
     catch(e) {
-        console.log(e);
         res.redirect('/blog/write/');
     }
 });
@@ -96,7 +111,7 @@ app.post ('/welcome' , (req, res)=>{
     res.send('SUCCESS');
     })
 
-app.get ('/blog/:id/edit', (req, res)=>{
+app.get ('/blog/:id/edit', slimelover, (req, res)=>{
     BlogPost.findById({id: req.params.id}, (error, result)=>{
         if(error) res.redirect('/blog/')
         else if (!result) res.redirect('/blog/')
@@ -104,7 +119,7 @@ app.get ('/blog/:id/edit', (req, res)=>{
     })
 })
 
-app.post ('/blog/:id/edit', (req, res)=>{
+app.post ('/blog/:id/edit', slimelover,(req, res)=>{
     BlogPost.findById(req.params.id, (error, result)=>{
         if(error){
             console.log(error);
@@ -125,7 +140,7 @@ app.put('/blog/update', (req, res)=>{
 })
 
 
-app.get('/blog/:id/delete', (req, res)=>{
+app.get('/blog/:id/delete',slimelover, (req, res)=>{
     BlogPost.deleteOne({id: req.params.id}, (error, result)=>{
         if(error){
             console.log(error);
@@ -139,4 +154,86 @@ app.use((req, res, next)=>{
     next();
 })
 
+app.post('/blog/:id/comment', authenticated, (req, res)=>{
+    BlogPost.findById(req.params.id, (error, result)=>{
+        if(error) {
+            console.log(error);
+            res.send('Error');
+        }
+        else if(!result) {
+            res.redirect('/blog/');
+        }
+        else {
+            result.comments.push({author: req.session.username, text: req.body.comment});
+            result.save();
+            res.redirect(path.join('/blog/', req.params.id+'/'));
+        }
+    });
+});
+
+app.post('/blog/:id/deletecomment/:comment', slimelover, async (req, res)=>{
+    BlogPost.findById(req.params.id, (error, result)=>{
+        if(error) {
+            console.log(error);
+            res.redirect('/');
+        }
+        else if(!result) {
+            res.send('Did you just delete comment on a non-existant post?');
+        }
+        else {
+            result.comments.id(req.params.comment).remove();
+            result.save();
+            res.redirect('/blog/'+req.params.id+'/');
+        }
+    });
+});
+
+app.get ('/login',  (req, res)=>{
+    res.render('login', {data: req.session});
+});
+
+app.get('/register', (req, res) => {
+    res.render('register', {data: req.session});
+});
+app.post('/register', async (req, res)=>{
+    try {
+        let rawpass = req.body.password;
+        var hashedpass = await bcrypt.hash(rawpass, 10);
+        var user = new User(req.body);
+        user.password = hashedpass;
+        await user.save();
+        res.redirect('/login');
+    }
+    catch(e) {
+        console.log(e);
+        res.send("Unable to register!");
+    }
+})
+app.post('/login', (req, res)=>{
+    User.findOne({username: req.body.username}, async (error, result)=>{
+        if(error) {
+            console.log(error);
+            res.send("!");
+        }
+        else if(!result) res.send("User not found.");
+        else {
+            try {
+                let match = await bcrypt.compare(req.body.password, result.password);
+                if(match) {
+                    req.session.username = result.username;
+                    req.session.authenticated = true;
+                    req.session.isSlimeLover = result.isSlimeLover;
+                    res.redirect('/blog/');
+                }
+                else res.send('Incorrect password');
+            }
+            catch(e) {
+                console.log(e);
+                res.send('Error');
+            }
+
+        }
+    })
+    
+})
 app.listen(2000);
--- server/models.js
+++ server/models.js
@@ -1,21 +1,66 @@
 const mongoose= require('mongoose');
 
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    favorite: {
+        type: String
+    },
+    isSlimeLover: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const commentSchema = new mongoose.Schema(
+    {
+        author: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: {
+            createdAt: "created_at"
+        }
+    }
+);
+
+
 const blogpostSchema = new mongoose.Schema({
-    title:{
+    title: {
         type: String,
         required: true,
         unique: true
     },
-    body:{
+    body: {
         type: String,
         required: true
-    }
+    },
+    author: {
+        type: String,
+        required: true,
+        default: "SlimeLover"
+    },
+    comments: [commentSchema]
 },{
-    timestamps:{
-        createdAt: "created_At"
+    timestamps: {
+        createdAt: "created_at"
     }
 });
 
+const User = mongoose.model("User",userSchema,"users");
 const BlogPost = mongoose.model("BlogPost" , blogpostSchema, "blogposts");
 
-module.exports = { BlogPost };
+module.exports = { BlogPost, User };
